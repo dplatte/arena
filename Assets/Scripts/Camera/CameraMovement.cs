1c6ba9f5df0ec234771a9b77dbd5f12e11ab42bc
Dustin Platte <dustinplatte@gmail.com>
2017-01-24 22:45:12 -0800

    Initial code commit.

---
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CameraMovement : MonoBehaviour {
+
+	public GameObject target;                           // Target to follow
+    public float targetHeight = 1.7f;                         // Vertical offset adjustment
+    public float distance = 12.0f;                            // Default Distance
+    public float offsetFromWall = 0.1f;                       // Bring camera away from any colliding objects
+    float maxDistance = 20f;                       // Maximum zoom Distance
+    float minDistance = 0.6f;                      // Minimum zoom Distance
+    float xSpeed = 200.0f;                             // Orbit speed (Left/Right)
+    float ySpeed = 200.0f;                             // Orbit speed (Up/Down)
+    float yMinLimit = -80f;                            // Looking up limit
+    float yMaxLimit = 80f;                             // Looking down limit
+    float zoomRate = 40f;                          // Zoom Speed
+    float rotationDampening = 3.0f;                // Auto Rotation speed (higher = faster)
+    float zoomDampening = 5.0f;                    // Auto Zoom speed (Higher = faster)
+    public LayerMask collisionLayers = -1;     // What the camera will collide with
+    public bool lockToRearOfTarget = false;             // Lock camera to rear of target
+    bool allowMouseInputX = true;                // Allow player to control camera angle on the X axis (Left/Right)
+    bool allowMouseInputY = true;                // Allow player to control camera angle on the Y axis (Up/Down)
+     
+    private float xDeg = 0.0f;
+    private float yDeg = 0.0f;
+    private float currentDistance;
+    private float desiredDistance;
+    private float correctedDistance;
+    private bool rotateBehind = false; 
+
+	private bool holdingLeftClick = false;
+         
+	void Start () {      
+		Vector3 angles = transform.eulerAngles;
+		xDeg = angles.x;
+		yDeg = angles.y;
+		currentDistance = distance;
+		desiredDistance = distance;
+		correctedDistance = distance;
+       
+		// Make the rigid body not change rotation
+		if (GetComponent<Rigidbody> ()) {
+			GetComponent<Rigidbody> ().freezeRotation = true;
+		}
+           
+		if (lockToRearOfTarget) {
+			rotateBehind = true;
+		}
+	}
+
+    void Update(){
+        if (target == null){
+            target = GameObject.FindGameObjectWithTag("Player") as GameObject;
+            Debug.Log("Looking for Player");
+        }
+ 
+    }
+         
+    //Only Move camera after everything else has been updated
+    void LateUpdate () {
+        // Don't do anything if target is not defined
+		if (target == null) {
+			return;
+		}
+       
+        Vector3 vTargetOffset;
+                   
+        // If either mouse buttons are down, let the mouse govern camera position
+        if (GUIUtility.hotControl == 0) {
+            if (Input.GetMouseButton(0)) {
+				if (!holdingLeftClick) {
+					holdingLeftClick = true;
+				}
+                //Check to see if mouse input is allowed on the axis
+				if (allowMouseInputX) {
+					xDeg += Input.GetAxis ("Mouse X") * xSpeed * 0.02f;
+				} else {
+					RotateBehindTarget ();
+				}
+                if (allowMouseInputY)
+                    yDeg -= Input.GetAxis ("Mouse Y") * ySpeed * 0.02f;
+               
+                //Interrupt rotating behind if mouse wants to control rotation
+                if (!lockToRearOfTarget)
+                    rotateBehind = false;
+			} else if (Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0 || rotateBehind) {
+				// otherwise, ease behind the target if any of the directional keys are pressed
+                RotateBehindTarget();
+            }
+        }
+        yDeg = ClampAngle (yDeg, yMinLimit, yMaxLimit);
+     
+        // Set camera rotation
+        Quaternion rotation = Quaternion.Euler (yDeg, xDeg, 0);
+     
+        // Calculate the desired distance
+        desiredDistance -= Input.GetAxis ("Mouse ScrollWheel") * Time.deltaTime * zoomRate * Mathf.Abs (desiredDistance);
+        desiredDistance = Mathf.Clamp (desiredDistance, minDistance, maxDistance);
+        correctedDistance = desiredDistance;
+     
+        // Calculate desired camera position
+        vTargetOffset = new Vector3 (0, -targetHeight, 0);
+        Vector3 position = target.transform.position - (rotation * Vector3.forward * desiredDistance + vTargetOffset);
+     
+        // Check for collision using the true target's desired registration point as set by user using height
+        RaycastHit collisionHit;
+        Vector3 trueTargetPosition = new Vector3 (target.transform.position.x, target.transform.position.y + targetHeight, target.transform.position.z);
+     
+        // If there was a collision, correct the camera position and calculate the corrected distance
+        var isCorrected = false;
+        if (Physics.Linecast (trueTargetPosition, position, out collisionHit, collisionLayers))
+        {
+            // Calculate the distance from the original estimated position to the collision location,
+            // subtracting out a safety "offset" distance from the object we hit.  The offset will help
+            // keep the camera from being right on top of the surface we hit, which usually shows up as
+            // the surface geometry getting partially clipped by the camera's front clipping plane.
+            correctedDistance = Vector3.Distance (trueTargetPosition, collisionHit.point) - offsetFromWall;
+            isCorrected = true;
+        }
+     
+        // For smoothing, lerp distance only if either distance wasn't corrected, or correctedDistance is more than currentDistance
+        currentDistance = !isCorrected || correctedDistance > currentDistance ? Mathf.Lerp (currentDistance, correctedDistance, Time.deltaTime * zoomDampening) : correctedDistance;
+     
+        // Keep within limits
+        currentDistance = Mathf.Clamp (currentDistance, minDistance, maxDistance);
+     
+        // Recalculate position based on the new currentDistance
+        position = target.transform.position - (rotation * Vector3.forward * currentDistance + vTargetOffset);
+       
+        //Finally Set rotation and position of camera
+        transform.rotation = rotation;
+        transform.position = position;
+    }
+     
+    private void RotateBehindTarget() {
+        float targetRotationAngle = target.transform.eulerAngles.y;
+        float currentRotationAngle = transform.eulerAngles.y;
+
+		//Need to enable it like this for when holding the left mouse button down and then letting go
+		if (!holdingLeftClick) {
+			xDeg = targetRotationAngle;
+		} else {
+			xDeg = Mathf.LerpAngle (currentRotationAngle, targetRotationAngle, rotationDampening * Time.deltaTime);
+		}
+        // Stop rotating behind if not completed
+		if (Math.Abs(currentRotationAngle - targetRotationAngle) < 1) {
+			holdingLeftClick = false;
+			if (!lockToRearOfTarget) {
+				rotateBehind = false;
+			}
+		} else {
+			//rotateBehind = true;
+		}
+     
+    }
+
+	private float ClampAngle (float angle, float min, float max) {
+		if (angle < -360f) {
+			angle += 360f;
+		}
+		if (angle > 360f) {
+			angle -= 360f;
+		}
+       	return Mathf.Clamp (angle, min, max);
+    }
+}
---
+++ Assets/Scripts/Player/BasePlayerController.cs
@@ -0,0 +1,472 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasePlayerController : MonoBehaviour {
+
+	#region Variables
+
+	//Components
+	Rigidbody rb;
+	protected Animator animator;
+	public GameObject target;
+
+	//jumping variables
+	public float gravity = -9.8f;
+	bool canJump;
+	bool isJumping = false;
+	bool isGrounded;
+	public float jumpSpeed = 500f;
+	bool isFalling;
+	bool startFall;
+	float fallingVelocity = -1f;
+
+	// Used for continuing momentum while in air
+	public float inAirSpeed = 8f;
+	float maxVelocity = 2f;
+	float minVelocity = -2f;
+
+	//movement variables
+	bool canMove = true;
+	public float walkSpeed = 1.35f;
+	float moveSpeed;
+	public float runSpeed = 6f;
+	float rotationSpeed = 100f;
+	public float mouseRotationSpeed = 300f;
+	public float keyboardRotationSpeed = 100f;
+
+	float x;
+	float z;
+	float dv;
+	float dh;
+	Vector3 moveVec;
+	Vector3 strafeVec;
+
+	Vector3 newVelocity;
+
+	//Weapon and Shield
+	private Weapon weapon;
+	int rightWeapon = 0;
+	int leftWeapon = 0;
+	bool isRelax = false;
+
+	//isStrafing/action variables
+	bool canAction = true;
+	bool isStrafing = false;
+	bool isDead = false;
+	bool isBlocking = false;
+	public float knockbackMultiplier = 1f;
+	bool isKnockback;
+
+	#endregion
+
+	#region Initialization
+
+	void Start() {
+		//set the animator component
+		animator = GetComponentInChildren<Animator>();
+		rb = GetComponent<Rigidbody>();
+	}
+
+	#endregion
+
+	#region UpdateAndInput
+
+	void Update() {
+
+		//make sure there is animator on character
+		if(animator) {
+			if(canMove && !isBlocking && !isDead) {
+				CameraRelativeMovement();
+			} 
+
+			Jumping();
+
+			if(Input.GetButtonDown("Death") && canAction && isGrounded && !isBlocking) {
+				if(!isDead) {
+					StartCoroutine(_Death());
+				} else {
+					StartCoroutine(_Revive());
+				}
+			}
+
+			//if strafing
+			if(Input.GetKey(KeyCode.LeftShift) || Input.GetAxisRaw("TargetBlock") > .1 && canAction) {  
+				isStrafing = true;
+				animator.SetBool("Strafing", true);
+				if(Input.GetButtonDown("CastL") && canAction && isGrounded && !isBlocking) {
+					CastAttack(1);
+				}
+				if(Input.GetButtonDown("CastR") && canAction && isGrounded && !isBlocking) {
+					CastAttack(2);
+				}
+			} else {
+				isStrafing = false;
+				animator.SetBool("Strafing", false);
+			}
+		} else {
+			Debug.Log("ERROR: There is no animator for character.");
+		}
+	}
+
+	#endregion
+
+	#region Fixed/Late Updates
+
+	void FixedUpdate() {
+		CheckForGrounded();
+		//apply gravity force
+		rb.AddForce(0, gravity, 0, ForceMode.Acceleration);
+		AirControl();
+		//check if character can move
+		if(canMove) {
+			moveSpeed = UpdateMovement();  
+		}
+		//check if falling
+		if(rb.velocity.y < fallingVelocity) {
+			isFalling = true;
+			animator.SetInteger("Jumping", 2);
+			canJump = false;
+		} else {
+			isFalling = false;
+		}
+	}
+
+	//get velocity of rigid body and pass the value to the animator to control the animations
+	void LateUpdate() {
+		//Get local velocity of charcter
+		float velocityXel = transform.InverseTransformDirection(rb.velocity).x;
+		float velocityZel = transform.InverseTransformDirection(rb.velocity).z;
+
+		Debug.Log ("Velocity X: " + velocityXel.ToString ());
+		Debug.Log ("Velocity Z: " + velocityZel.ToString ());
+		//Update animator with movement values
+		animator.SetFloat("Velocity X", runSpeed);
+		animator.SetFloat("Velocity Z", runSpeed);
+		//if character is alive and can move, set our animator
+		if(!isDead && canMove) {
+			if(moveSpeed > 0) {
+				animator.SetBool("Moving", true);
+			} else {
+				animator.SetBool("Moving", false);
+			}
+		}
+	}
+
+	#endregion
+
+	#region UpdateMovement
+
+	void CameraRelativeMovement(){
+		float inputTurnCharacter = Input.GetAxisRaw("Turn Character");
+		float inputMoveCharacter = Input.GetAxisRaw("Move Character");
+		float inputStrafeCharacter = Input.GetAxisRaw ("Strafe Character");
+
+		if (Input.GetMouseButton (1)) {
+			inputTurnCharacter = Input.GetAxisRaw ("Mouse X");
+			rotationSpeed = mouseRotationSpeed;
+		} else {
+			rotationSpeed = keyboardRotationSpeed;
+		}
+
+		transform.Rotate (0, inputTurnCharacter * rotationSpeed * Time.deltaTime, 0);
+
+		//converts control input vectors into camera facing vectors
+
+		//Forward vector relative to the camera along the x-z plane   
+		Vector3 forward = transform.TransformDirection(Vector3.forward);
+		forward.y = 0;
+		forward = forward.normalized;
+
+		Vector3 right = transform.TransformDirection(Vector3.right);
+		right.y = 0;
+		right = right.normalized;
+		//directional inputs
+
+		moveVec = inputMoveCharacter * forward;
+		strafeVec = inputStrafeCharacter * right;
+	}
+
+	public static float ClampAngle(float angle, float min, float max) {
+		if (angle < -360F) {
+			angle += 360F;
+		}
+		if (angle > 360F) {
+			angle -= 360F;
+		}
+		return Mathf.Clamp(angle, min, max);
+	}
+
+	float UpdateMovement() {
+		//CameraRelativeMovement();  
+		Vector3 motion = moveVec;
+		if(isGrounded) {
+			//reduce input for diagonal movement
+			if(motion.magnitude > 1) {
+				motion.Normalize();
+			}
+			if(canMove) {
+				//set speed by walking / running
+				if(isStrafing) {
+					newVelocity = motion * walkSpeed;
+				} else {
+					newVelocity = motion * runSpeed;
+				}
+			}
+		} else {
+			//if we are falling use momentum
+			//newVelocity = rb.velocity;
+		}
+		//newVelocity.y = rb.velocity.y;
+		//rb.velocity = newVelocity;
+
+		float inputMoveCharacter = Input.GetAxisRaw("Move Character");
+		float inputStrafeCharacter = Input.GetAxisRaw ("Strafe Character");
+
+		rb.velocity = new Vector3(inputStrafeCharacter, 0.0f, inputMoveCharacter) * runSpeed;
+
+		//rb.MovePosition(transform.position + inputMoveCharacter * transform.forward * runSpeed * Time.deltaTime);
+		//return a movement value for the animator
+		return moveVec.magnitude;
+	}
+
+	#endregion
+
+	#region Jumping
+
+	//checks if character is within a certain distance from the ground, and markes it IsGrounded
+	void CheckForGrounded() {
+		float distanceToGround;
+		float threshold = .45f;
+		RaycastHit hit;
+		Vector3 offset = new Vector3(0,.4f,0);
+		if(Physics.Raycast((transform.position + offset), -Vector3.up, out hit, 100f)) {
+			distanceToGround = hit.distance;
+			if(distanceToGround < threshold) {
+				isGrounded = true;
+				canJump = true;
+				startFall = false;
+				isFalling = false;
+				if(!isJumping) {
+					animator.SetInteger("Jumping", 0);
+				}
+			} else {
+				isGrounded = false;
+			}
+		}
+	}
+
+	void Jumping() {
+		if(isGrounded) {
+			if(canJump && Input.GetButtonDown("Jump")) {
+				StartCoroutine(_Jump());
+			}
+		} else {    
+			canJump = false;
+			if(isFalling) {
+				//set the animation back to falling
+				animator.SetInteger("Jumping", 2);
+				//prevent from going into land animation while in air
+				if(!startFall) {
+					animator.SetTrigger("JumpTrigger");
+					startFall = true;
+				}
+			}
+		}
+	}
+
+	IEnumerator _Jump() {
+		isJumping = true;
+		animator.SetInteger("Jumping", 1);
+		animator.SetTrigger("JumpTrigger");
+		// Apply the current movement to launch velocity
+		//rb.velocity += jumpSpeed * Vector3.up;
+
+		rb.velocity += Vector3.up * jumpSpeed;
+		canJump = false;
+		yield return new WaitForSeconds(.5f);
+		isJumping = false;
+	}
+
+	void AirControl() {
+		if(!isGrounded) {
+			//CameraRelativeMovement();
+			Vector3 motion = moveVec;
+			motion *= (Mathf.Abs(moveVec.x) == 1 && Mathf.Abs(moveVec.z) == 1) ? 0.7f:1;
+			rb.AddForce(motion * inAirSpeed, ForceMode.Acceleration);
+			//limit the amount of velocity we can achieve
+			float velocityX = 0;
+			float velocityZ = 0;
+			if(rb.velocity.x > maxVelocity) {
+				velocityX = GetComponent<Rigidbody>().velocity.x - maxVelocity;
+				if(velocityX < 0)
+				{
+					velocityX = 0;
+				}
+				rb.AddForce(new Vector3(-velocityX, 0, 0), ForceMode.Acceleration);
+			}
+			if(rb.velocity.x < minVelocity) {
+				velocityX = rb.velocity.x - minVelocity;
+				if(velocityX > 0) {
+					velocityX = 0;
+				}
+				rb.AddForce(new Vector3(-velocityX, 0, 0), ForceMode.Acceleration);
+			}
+			if(rb.velocity.z > maxVelocity) {
+				velocityZ = rb.velocity.z - maxVelocity;
+				if(velocityZ < 0) {
+					velocityZ = 0;
+				}
+				rb.AddForce(new Vector3(0, 0, -velocityZ), ForceMode.Acceleration);
+			}
+			if(rb.velocity.z < minVelocity) {
+				velocityZ = rb.velocity.z - minVelocity;
+				if(velocityZ > 0) {
+					velocityZ = 0;
+				}
+				rb.AddForce(new Vector3(0, 0, -velocityZ), ForceMode.Acceleration);
+			}
+		}
+	}
+
+	#endregion
+
+	#region MiscMethods
+
+	//0 = No side
+	//1 = Left
+	//2 = Right
+	//3 = Dual
+	void Attack(int attackSide) {
+		if(canAction) {
+			if(weapon == Weapon.UNARMED) {
+				int maxAttacks = 3;
+				int attackNumber = 0;
+				if(attackSide == 1 || attackSide == 3) {
+					attackNumber = Random.Range(3, maxAttacks);
+				} else if(attackSide == 2) {
+					attackNumber = Random.Range(6, maxAttacks + 3);
+				}
+				if(isGrounded) {
+					if(attackSide != 3) {
+						animator.SetTrigger("Attack" + (attackNumber).ToString() + "Trigger");
+						if(leftWeapon == 12 || leftWeapon == 14 || rightWeapon == 13 || rightWeapon == 15) {
+							StartCoroutine(_LockMovementAndAttack(0, .75f));
+						} else {
+							StartCoroutine(_LockMovementAndAttack(0, .6f));
+						}
+					} else {
+						animator.SetTrigger("AttackDual" + (attackNumber).ToString() + "Trigger");
+						StartCoroutine(_LockMovementAndAttack(0, .75f));
+					}
+				}
+			} else {
+				if(isGrounded) {
+					animator.SetTrigger("Attack" + (6).ToString() + "Trigger");
+					StartCoroutine(_LockMovementAndAttack(0, .85f));
+				}
+			}
+		}
+	}
+
+	void AttackKick(int kickSide) {
+		if(isGrounded) {
+			if(kickSide == 1) {
+				animator.SetTrigger("AttackKick1Trigger");
+			} else {
+				animator.SetTrigger("AttackKick2Trigger");
+			}
+			StartCoroutine(_LockMovementAndAttack(0, .8f));
+		}
+	}
+
+	//0 = No side
+	//1 = Left
+	//2 = Right
+	//3 = Dual
+	void CastAttack(int attackSide) {
+		if(weapon == Weapon.UNARMED) {
+			int maxAttacks = 3;
+			if(attackSide == 1) {
+				int attackNumber = Random.Range(0, maxAttacks);
+				if(isGrounded) {
+					animator.SetTrigger("CastAttack" + (attackNumber + 1).ToString() + "Trigger");
+					StartCoroutine(_LockMovementAndAttack(0, .8f));
+				}
+			}
+			if(attackSide == 2) {
+				int attackNumber = Random.Range(3, maxAttacks + 3);
+				if(isGrounded) {
+					animator.SetTrigger("CastAttack" + (attackNumber + 1).ToString() + "Trigger");
+					StartCoroutine(_LockMovementAndAttack(0, .8f));
+				}
+			}
+			if(attackSide == 3) {
+				int attackNumber = Random.Range(0, maxAttacks);
+				if(isGrounded) {
+					animator.SetTrigger("CastDualAttack" + (attackNumber + 1).ToString() + "Trigger");
+					StartCoroutine(_LockMovementAndAttack(0, 1f));
+				}
+			}
+		} 
+	}
+
+	IEnumerator _Knockback(Vector3 knockDirection, int knockBackAmount, int variableAmount) {
+		isKnockback = true;
+		StartCoroutine(_KnockbackForce(knockDirection, knockBackAmount, variableAmount));
+		yield return new WaitForSeconds(.1f);
+		isKnockback = false;
+	}
+
+	IEnumerator _KnockbackForce(Vector3 knockDirection, int knockBackAmount, int variableAmount) {
+		while(isKnockback) {
+			rb.AddForce(knockDirection * ((knockBackAmount + Random.Range(-variableAmount, variableAmount)) * (knockbackMultiplier * 10)), ForceMode.Impulse);
+			yield return null;
+		}
+	}
+
+	IEnumerator _Death() {
+		animator.SetTrigger("Death1Trigger");
+		StartCoroutine(_LockMovementAndAttack(.1f, 1.5f));
+		isDead = true;
+		animator.SetBool("Moving", false);
+		moveVec = new Vector3(0, 0, 0);
+		yield return null;
+	}
+
+	IEnumerator _Revive() {
+		animator.SetTrigger("Revive1Trigger");
+		isDead = false;
+		yield return null;
+	}
+
+	#endregion
+
+	#region _Coroutines
+
+	//method to keep character from moveing while attacking, etc
+	public IEnumerator _LockMovementAndAttack(float delayTime, float lockTime) {
+		yield return new WaitForSeconds(delayTime);
+		canAction = false;
+		canMove = false;
+		animator.SetBool("Moving", false);
+		rb.velocity = Vector3.zero;
+		rb.angularVelocity = Vector3.zero;
+		moveVec = new Vector3(0, 0, 0);
+		animator.applyRootMotion = true;
+		yield return new WaitForSeconds(lockTime);
+		canAction = true;
+		canMove = true;
+		animator.applyRootMotion = false;
+	}
+
+	#endregion
+
+	#region GUI
+
+	void OnGUI() {
+		
+	}
+
+	#endregion
+}
