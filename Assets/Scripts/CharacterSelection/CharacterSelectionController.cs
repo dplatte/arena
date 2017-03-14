b651383cc4af7a3eb73527e12cf4c55b502a292a
Dustin Platte <dustinplatte@gmail.com>
2017-03-13 23:13:06 -0700

    Added initial login screen and character selection screen.

---
+++ Assets/Scripts/CharacterSelection/CharacterSelectionController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacterSelectionController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnGUI() {
+		if (GUI.Button (new Rect (Screen.width / 2 - 50, Screen.height - 40, 100, 20), "Enter World")) {
+			SceneManager.LoadScene("Arena");
+		}
+	}
+}
---
+++ Assets/Scripts/Login/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoginController : MonoBehaviour {
+
+	string username = "Username";
+	string password = "Password";
+
+	bool Login() {
+		if (username != "" && password != "") {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	void OnGUI() {
+		username = GUI.TextField (new Rect (Screen.width / 2 - 50, Screen.height - 100, 100, 20), username);
+		password = GUI.TextField (new Rect (Screen.width / 2 - 50, Screen.height - 70, 100, 20), password);
+		if (GUI.Button (new Rect (Screen.width / 2 - 50, Screen.height - 40, 100, 20), "Login")) {
+			if (Login ()) {
+				SceneManager.LoadScene("CharacterSelection");
+			} else {
+				Debug.Log ("Login invalid");
+			}
+		}
+	}
+}
